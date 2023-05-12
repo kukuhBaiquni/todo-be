57aebfbcaa3c1c370d657b332fbec3b2ff6e0388
kukuhbaiquni <kukuh.baiquni@gmail.com>
2023-05-13 00:18:47 +0700

    fix endpoint path

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -3,12 +3,12 @@ const controller = require('../controller/todo')
 
 const router = express.Router()
 
-router.post('/create', controller.createTodo)
+router.post('/', controller.createTodo)
 router.get('/:id', controller.getTodo)
 router.delete('/:id', controller.deleteTodo)
 router.put('/:id', controller.updateTodo)
-router.delete('/:todoId/todo-list/:todoListId', controller.deleteListTodo)
-router.put('/:todoId/todo-list/:todoListId', controller.updateListTodo)
-router.post('/create/todo-list', controller.createTodoList)
+router.delete('/:todoId/:todoListId', controller.deleteListTodo)
+router.put('/:todoId/:todoListId', controller.updateListTodo)
+router.post('/todo-list', controller.createTodoList)
 
 module.exports = router
